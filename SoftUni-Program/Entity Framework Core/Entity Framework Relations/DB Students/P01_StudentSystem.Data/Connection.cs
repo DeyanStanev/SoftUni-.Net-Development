6d69aa63308601db85eca7c39bba83bacfa0d1d8
Deyan Stanev <73663872+DeyanStanev@users.noreply.github.com>
2021-10-31 10:01:18 +0200

    Entity-students

---
+++ SoftUni-Program/Entity Framework Core/Entity Framework Relations/DB Students/P01_StudentSystem.Data/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public static class Connection
+    {
+        public const string CONECTION = @"Server=(localdb)\MSSQLLocalDB;Database=StudentSystem;Integrated Security=True;";
+    }
+}
---
+++ SoftUni-Program/Entity Framework Core/Entity Framework Relations/DB Students/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext: DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+        public StudentSystemContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<StudentCourse>().HasKey(k => new { k.StudentId, k.CourseId });
+            modelBuilder.Entity<Student>().Property(p => p.PhoneNumber).IsUnicode(false);
+            modelBuilder.Entity<Resource>().Property(p => p.Url).IsUnicode(false);
+            modelBuilder.Entity<Homework>().Property(p => p.Content).IsUnicode(false);
+
+        }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions{ get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+    }
+}
